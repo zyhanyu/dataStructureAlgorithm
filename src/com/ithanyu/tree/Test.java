3064c9aad5e3b44a96c9c637a0faa6ffc39052f6
涵堉 <10601694+culverthanyu@user.noreply.gitee.com>
2023-11-21 20:30:07 +0800

    了解树的概念并对各数据结构进行测试和比较

---
+++ src/com/ithanyu/tree/Test.java
@@ -0,0 +1,22 @@
+package com.ithanyu.tree;
+
+import java.util.ArrayList;
+
+public class Test {
+
+	@SuppressWarnings("unused")
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		@SuppressWarnings("rawtypes")
+		// ArrayList 维护了数组 transient Object
+		ArrayList arrayList = new ArrayList();
+	}
+
+}
+
+
+
+
+
+
+
