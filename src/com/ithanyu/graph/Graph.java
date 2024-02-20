09f10690f703017ae44c704500deb7f1a9745d0a
涵堉 <10601694+culverthanyu@user.noreply.gitee.com>
2024-02-20 16:49:34 +0800

    图完成深度优先算法

--- src/com/ithanyu/graph/Graph.java
+++ src/com/ithanyu/graph/Graph.java
@@ -5,6 +5,8 @@ public class Graph {
 	private ArrayList<String> vertexList;  // 存储顶点集合
 	private int[][] edges; // 存储图对应的邻接矩阵
 	private int numOfEdges; // 表示边的数目
+	// 定义给数组boolean[],记录某个结点是否被访问
+	private boolean[] isVisited;
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
@@ -27,6 +29,10 @@ public class Graph {
 		
 		// 显示一把邻接矩阵
 		graph.showGraph();
+		
+		// 测试一把，我们的dfs遍历是否ok
+		System.out.println("深度遍历");
+		graph.dfs();
 	}
 	
 	// 构造器
@@ -35,6 +41,58 @@ public class Graph {
 		edges = new int[n][n];
 		vertexList = new ArrayList<String>(n);
 		numOfEdges = 0;
+		isVisited = new boolean[5];
+	}
+	// 得到第一个邻接结点的下标 w
+	/**
+	 * 
+	 * @param index
+	 * @return 如果存在就返回对应的下标，否则返回-1
+	 */
+	public int getFirstNeighbor(int index){
+		for(int j=0;j<vertexList.size();j++){
+			if(edges[index][j]>0){
+				return j;
+			}
+		}
+		return -1;
+	}
+	// 根据前一个邻接结点的下标来获取下一个邻接结点
+	public int getNextNeighbor(int v1,int v2){
+		for(int j = v2 + 1 ;j<vertexList.size();j++){
+			if(edges[v1][j] > 0){
+				return j;
+			}
+		}
+		return -1;
+	}
+	
+	// 深度优先遍历算法
+	// i 第一次就是 0
+	public void dfs(boolean[] isVisited,int i){
+		// 首先我们访问该节点，输出
+		System.out.print(getValueByIndex(i) + "->");
+		// 将结点设置为已经访问
+		isVisited[i] = true;
+		// 查找结点i的第一个邻接结点w
+		int w = getFirstNeighbor(i);
+		while(w != -1){ // 说明有
+			if(!isVisited[w]){
+				dfs(isVisited,w);
+			}
+			// 如果w结点已经被访问过
+			w = getNextNeighbor(i,w);
+		}
+	}
+	
+	// 对dfs 进行一个重载,遍历我们所有的结点，并进行 dfs
+	private void dfs(){
+		// 遍历所有的结点，进行dfs[回溯]
+		for(int i=0;i<getNumOfVertex();i++){
+			if(!isVisited[i]){
+				dfs(isVisited,i);
+			}
+		}
 	}
 	
 	// 图中常用的方法
